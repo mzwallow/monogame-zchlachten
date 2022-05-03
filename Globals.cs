f2c06d5f83302ca6072fd562a67422cc2dbe0adc
Punnarat Rattanapawan <punnarat_mos@hotmail.com>
2022-05-03 22:36:16 +0700

    :tada: Initial commit

---
+++ Globals.cs
@@ -0,0 +1,8 @@
+namespace Zchlachten
+{
+    public static class Globals
+    {
+        public static int SCREEN_WIDTH { get; } = 1280;
+        public static int SCREEN_HEIGHT { get; } = 720;
+    }
+}

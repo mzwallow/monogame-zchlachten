b5cd5138f7b2240c317cdaab077269ca3b36b182
Shikino-code <billso2000@gmail.com>
2022-05-20 21:14:06 +0700

    :hammer: convert effect in camera2D

--- Entities/StatusEffect.cs
+++ Entities/StatusEffect.cs
@@ -1,6 +1,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using tainicom.Aether.Physics2D.Dynamics;
+using tainicom.Aether.Physics2D.Dynamics.Contacts;
 
 namespace Zchlachten.Entities
 {
@@ -10,9 +11,12 @@ namespace Zchlachten.Entities
 
         private Texture2D _texture;
         private Vector2 _textureOrigin;
-        private Body _body;
-
+        public Body Body;
+        private Fixture _fixture;
         public StatusEffectType statusEffectType;
+        public bool HasCollided = false;
+        private Vector2 _size;
+        private Vector2 _scale;
 
         public StatusEffect(World world, Texture2D texture, Vector2 position)
         {
@@ -21,9 +25,12 @@ namespace Zchlachten.Entities
             _texture = texture;
             _textureOrigin = new Vector2(_texture.Width / 2, _texture.Height / 2);
 
-            _body = _world.CreateBody(position, 0f, BodyType.Static);
+            _size = new Vector2(_texture.Width*0.0234375f, _texture.Height*0.0234375f)*1.25f;
+            _scale = _size / new Vector2(_texture.Width, _texture.Height);
+            
+            Body = _world.CreateBody(position);
 
-            var statusEffectFixture = _body.CreateCircle(_texture.Width, 1f);
+            var statusEffectFixture = Body.CreateCircle(_texture.Width, 1f);
             statusEffectFixture.Tag = "statusEffects";
         }
 
@@ -33,15 +40,23 @@ namespace Zchlachten.Entities
         {
             spriteBatch.Draw(
                 _texture,
-                _body.Position,
+                Body.Position,
                 null,
                 Color.White,
-                _body.Rotation,
+                Body.Rotation,
                 _textureOrigin,
-                1f,
-                SpriteEffects.None,
+                _scale,
+                SpriteEffects.FlipVertically,
                 0f
             );
         }
+
+        private bool OnCollisionEventHandler(Fixture sender, Fixture other, Contact contact)
+        {
+            HasCollided = true;
+            
+            return true;
+        }
+
     }
 }
